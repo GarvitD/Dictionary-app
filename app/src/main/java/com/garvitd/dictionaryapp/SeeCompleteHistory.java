0700a5f8a0acf459a97956d99e82b9ef80e57d77
garvi <garvitgd11@gmail.com>
2021-10-01 23:53:21 +0530

    some new features like word of the day, speech search along with image search and some more have been added.

---
+++ app/src/main/java/com/garvitd/dictionaryapp/SeeCompleteHistory.java
@@ -0,0 +1,14 @@
+package com.garvitd.dictionaryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeeCompleteHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_see_complete_history);
+    }
+}
